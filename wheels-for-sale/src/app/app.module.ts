1c893e15a004bd51ae07d74b57b1f0bc3a7037b1
ram <sivaraman1296@gmail.com>
2018-12-27 16:44:06 +0530

    3 components and services created

--- wheels-for-sale/src/app/app.module.ts
+++ wheels-for-sale/src/app/app.module.ts
@@ -1,18 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { HttpClientModule } from '@angular/common/http';
+ 
 
 import { AppComponent } from './app.component';
-
+import { TopComponent } from './top/top.component';
+import { CenterComponent } from './center/center.component';
+import { LeftComponent } from './left/left.component';
+import {SaleService} from './sale.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopComponent,
+    CenterComponent,
+    LeftComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    NgModule
   ],
-  providers: [],
+  providers: [SaleService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
