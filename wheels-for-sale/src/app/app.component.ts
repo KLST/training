3830c6f773f6ace2bb52bf7a48e91b90e212019a
BalajiKanan <balajikanan@klstinc.com>
2019-01-02 17:01:57 +0530

    navbar and service

--- wheels-for-sale/src/app/app.component.ts
+++ wheels-for-sale/src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
-import{Router} from '@angular/router';
-import {SaleService} from './service/sale.service';
+ 
+
 
 @Component({
   selector: 'app-root',
@@ -8,25 +8,14 @@ import {SaleService} from './service/sale.service';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  listvalues:any;
- 
+   
 
-constructor( public saleService: SaleService){
-this.getmethod();
-  
+constructor(  ){
+ 
+ 
 }
 
-
-
-
-getmethod(){
-this.saleService.getservicemethod().subscribe((res:any) =>{
-console.log(res);
-this.listvalues = res.value;
-console.log(this.listvalues)
-console.log(this.listvalues[4].Id )
-});
-} 
+ 
  
  
 }
